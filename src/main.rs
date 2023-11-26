71bb38b5ae5796bb75be77932d1dbbfce67da71e
Acheul <seoul605.21@gmail.com>
2023-11-26 14:44:09 +0900

    0.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() {
     .route("/api/html", get(html_handler))
     ;
 
-  axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+  axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
     .serve(app.into_make_service())
     .await
     .unwrap();
